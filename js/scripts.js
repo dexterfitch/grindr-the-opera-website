75d845a7dc117e42b560fd21c7fa7002fa0fefc1
Dex Fitch <fitch.dexter@gmail.com>
2015-04-21 18:06:07 -0400

    Change press articles to 'slick' carousel.

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+  $('.press-articles').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+});
